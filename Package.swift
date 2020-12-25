ffc45c5ed0384b1190f777e2a1262119fc42f088
Cong Nguyen <congnd@outlook.com>
2020-12-25 19:21:17 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "TouchPrinter",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "TouchPrinter",
+            targets: ["TouchPrinter"])
+    ],
+    targets: [
+        .target(name: "TouchPrinter"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
